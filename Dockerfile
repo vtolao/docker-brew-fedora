10ada29063147fde9e39190f4c2344b6e6e659e6
Lokesh Mandvekar <lsm5@fedoraproject.org>
2015-07-04 18:05:26 +0000

    fedora 20 semi-official image
    Resolves: upstream gh#807 (for fedora) - CVE-2015-4000 ("Logjam")
    
    - last update to Fedora 20 (now EOL)
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+MAINTAINER Lokesh Mandvekar <lsm5@fedoraproject.org> - ./buildcontainers.sh
+ADD fedora-20-medium.tar.xz /
---
+++ buildcontainers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+sudo rm -rf /tmp/fedora*
+rm *.tar.xz
+export LIBGUESTFS_BACKEND=direct
+sudo appliance-creator -c container-20-medium.ks -d -v -t /tmp \
+    -o /tmp --name "fedora-20-medium" --release 20 \
+    --format=qcow2
+virt-tar-out -a \
+    /tmp/fedora-20-medium/fedora-20-medium-sda.qcow2 / - | \
+    xz --best > fedora-20-medium.tar.xz
